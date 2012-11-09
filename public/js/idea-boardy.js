153648a54c104fece54492fe3ca2916270ea7353
swzhou <swing.zhou@hotmail.com>
2012-11-10 00:21:03 +0800

    [SW] format idea-boardy.js

--- public/js/idea-boardy.js
+++ public/js/idea-boardy.js
@@ -1,69 +1,71 @@
 angular.module('idea-boardy', ['ngResource'])
     .config(['$routeProvider', function ($routeProvider) {
-    $routeProvider.when('/boards/:boardId', {templateUrl:'template/board.html', controller:'BoardController'})
-    $routeProvider.otherwise({templateUrl:'template/board-list.html', controller:'BoardListController'})
-}])
+        $routeProvider.when('/boards/:boardId', {templateUrl:'template/board.html', controller:'BoardController'})
+        $routeProvider.otherwise({templateUrl:'template/board-list.html', controller:'BoardListController'})
+    }])
 
     .controller('BoardListController', ['$scope', '$resource',
-    function ($scope, $resource) {
-        var BoardResource = $resource('/boards/:boardId', {boardId:'@id'});
-        $scope.boards = BoardResource.query();
-    }
-])
+        function ($scope, $resource) {
+            var BoardResource = $resource('/boards/:boardId', {boardId:'@id'});
+            $scope.boards = BoardResource.query();
+        }
+    ])
 
     .controller('CreateBoardController', ['$scope', '$resource', '$route', '$http',
-    function ($scope, $resource, $route, $http) {
-        var BoardResource = $resource('/boards/:boardId'),
-            board = $scope.board = new BoardResource(),
-            sections = $scope.sections = [];
+        function ($scope, $resource, $route, $http) {
+            var BoardResource = $resource('/boards/:boardId'),
+                board = $scope.board = new BoardResource(),
+                sections = $scope.sections = [];
 
-        $scope.create = function () {
-            board.$save(function (createdBoard, responseHeaders) {
-                var createdBoardUri = responseHeaders('location');
-                $http.get(createdBoardUri).success(function (createdBoard) {
-                    var sectionsLink = _.find(createdBoard.links, function (l) {
-                        return l.rel == 'sections'
-                    });
-                    _.each(sections, function (section) {
-                        $http.post(sectionsLink.href, section);
+            $scope.create = function () {
+                board.$save(function (createdBoard, responseHeaders) {
+                    var createdBoardUri = responseHeaders('location');
+                    $http.get(createdBoardUri).success(function (createdBoard) {
+                        var sectionsLink = _.find(createdBoard.links, function (l) {
+                            return l.rel == 'sections'
+                        });
+                        _.each(sections, function (section) {
+                            $http.post(sectionsLink.href, section);
+                        });
                     });
                 });
-            });
-            $route.reload();
-        };
+                $route.reload();
+            };
 
-        $scope.addSection = function () {
-            sections.push({});
+            $scope.addSection = function () {
+                sections.push({});
+            }
         }
-    }
-])
+    ])
 
     .controller('SectionController', ['$scope', '$routeParams', '$http',
-    function ($scope, $http) {
-        $http({method:'GET', url:getLink($scope.section.links, 'ideas')})
-            .success(function (ideas) {
-                $scope.ideas = ideas;
-            });
-    }])
+        function ($scope, $http) {
+            $http({method:'GET', url:getLink($scope.section.links, 'ideas')})
+                .success(function (ideas) {
+                    $scope.ideas = ideas;
+                });
+        }
+    ])
 
     .controller('IdeaController', ['$scope', '$routeParams', '$http',
-    function ($scope, $http) {
-    }])
+        function ($scope, $http) {
+        }
+    ])
 
     .controller('BoardController', ['$scope', '$routeParams', '$resource', '$http',
-    function ($scope, $routeParams, $resource, $http) {
-        var BoardResource = $resource('/boards/:boardId'),
-            board, sections;
-        board = $scope.board = BoardResource.get({boardId:$routeParams.boardId}, function () {
-            var sectionsLink = _.find(board.links, function (l) {
-                return l.rel == 'sections'
+        function ($scope, $routeParams, $resource, $http) {
+            var BoardResource = $resource('/boards/:boardId'),
+                board, sections;
+            board = $scope.board = BoardResource.get({boardId:$routeParams.boardId}, function () {
+                var sectionsLink = _.find(board.links, function (l) {
+                    return l.rel == 'sections'
+                });
+                $http.get(sectionsLink.href).success(function (sections) {
+                    sections = $scope.sections = sections;
+                });
             });
-            $http.get(sectionsLink.href).success(function (sections) {
-                sections = $scope.sections = sections;
-            });
-        });
-    }
-]);
+        }
+    ]);
 
 
 function getLink(links, rel) {

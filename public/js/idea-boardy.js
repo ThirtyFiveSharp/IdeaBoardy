3d67859e029124c87b1ef7e80625aaf855520a92
TW-DIS-008 <TW-DIS-008@gmail.com>
2012-11-09 22:12:07 +0800

    [ZT&SW] board list

--- public/js/idea-boardy.js
+++ public/js/idea-boardy.js
@@ -1,24 +1,51 @@
-angular.module('idea-boardy', [])
+angular.module('idea-boardy', ['ngResource'])
     .config(['$routeProvider', function($routeProvider){
         $routeProvider.when('/boards/:boardId', {templateUrl: 'template/board.html', controller: 'BoardController'})
         $routeProvider.otherwise({templateUrl: 'template/board-list.html', controller: 'BoardListController'})
     }])
 
-    .controller('BoardListController', ['$scope',
-        function($scope) {
-            $scope.board = "We are boards!";
+    .controller('BoardListController', ['$scope', '$resource',
+        function($scope, $resource) {
+            var BoardResource = $resource('/boards/:boardId', {boardId: '@id'});
+            $scope.boards = BoardResource.query();
         }
     ])
 
+    .controller('CreateBoardController', ['$scope', '$resource', '$route', '$http',
+        function($scope, $resource, $route, $http) {
+            var BoardResource = $resource('/boards/:boardId'),
+                board = $scope.board = new BoardResource(),
+                sections = $scope.sections = [];
+
+            $scope.create = function() {
+                board.$save(function(createdBoard, responseHeaders) {
+                    var createdBoardUri = responseHeaders('location');
+                    $http.get(createdBoardUri).success(function(createdBoard) {
+                        var sectionsLink = _.find(createdBoard.links, function(l) {return l.rel == 'sections'});
+                        _.each(sections, function(section){
+                           $http.post(sectionsLink.href, section);
+                        });
+                    });
+                });
+                $route.reload();
+            };
+
+            $scope.addSection = function() {
+                sections.push({});
+            }
+        }
+    ])
 
     .controller('SectionController', ['$scope', '$routeParams', '$http',
         function($scope, $http) {
 
         }])
+
     .controller('IdeaController', ['$scope', '$routeParams', '$http',
         function($scope, $routeParams, $http) {
 
         }])
+
     .controller('BoardController', ['$scope', '$routeParams', '$http',
         function($scope, $routeParams, $http) {
             var sectionsLink,

6eba76b0c1d90bd3a3a9b69e42a0cc1057946d0f
swzhou <swing.zhou@hotmail.com>
2012-11-10 22:55:18 +0800

    [HZ] show ideas

--- public/js/idea-controller.js
+++ public/js/idea-controller.js
@@ -1,4 +1,14 @@
 ideaBoardModule.controller('IdeaController', ['$scope', '$http',
     function ($scope, $http) {
+        $http.get(getLink($scope.section.links, 'section').href).success(function (section) {
+            $http.get(getLink(section.links, 'ideas').href).success(function (ideas) {
+                $scope.ideas = ideas;
+            });
+        });
     }
 ]);
+
+function getLink(links, rel) {
+    var link = _.find(links, function(link) { return link.rel === rel; });
+    return link || {rel: null, href: null};
+}

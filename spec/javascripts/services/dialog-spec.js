04f70836b116fcc58af4e5d7066a0584e065be2b
Swing Zhou <swing.zhou@hotmail.com>
2012-11-24 18:27:03 +0800

    [SW] add unit tests for angular services

---
+++ spec/javascripts/services/dialog-spec.js
@@ -0,0 +1,44 @@
+describe('dialog', function () {
+
+    beforeEach(module('idea-boardy'));
+
+    it('should re-use same dialog when creating dialog with same name', inject(function (dialog) {
+        var dialog1 = dialog('dialog1');
+        var dialog2 = dialog('dialog1');
+        expect(dialog1).toBe(dialog2);
+    }));
+
+    it('should create new dialog when creating dialog with different name', inject(function (dialog) {
+        var dialog1 = dialog('dialog1');
+        var dialog2 = dialog('dialog2');
+        expect(dialog1).not.toBe(dialog2);
+    }));
+
+    it('should set dialog visible to true when open dialog', inject(function (dialog) {
+        var dialog = dialog('dialog');
+        dialog.open();
+        expect(dialog.visible).toBeTruthy();
+        expect(dialog.context).toEqual({});
+    }));
+
+    it('should set default context to {} when open dialog without context object', inject(function (dialog) {
+        var dialog = dialog('dialog');
+        dialog.open();
+        expect(dialog.context).toEqual({});
+    }));
+
+
+    it('should set dialog context to given context when open dialog', inject(function (dialog) {
+        var context = {name: 'value'};
+        var dialog = dialog('dialog');
+        dialog.open(context);
+        expect(dialog.context).toBe(context);
+    }));
+
+    it('should set dialog visible to false when close dialog', inject(function (dialog) {
+        var dialog = dialog('dialog');
+        dialog.open();
+        dialog.close();
+        expect(dialog.visible).toBeFalsy();
+    }));
+});
---
+++ spec/javascripts/services/params-spec.js
@@ -0,0 +1,20 @@
+describe('params', function () {
+    beforeEach(module('idea-boardy'));
+
+    it('should get parameter value', inject(function (params) {
+        params('key', 'value');
+        expect(params('key')).toBe('value');
+    }));
+
+    it('should set parameter value', inject(function (params) {
+        expect(params('key')).toBeUndefined();
+        params('key', 'value');
+        expect(params('key')).toBe('value');
+    }));
+
+    it('should override parameter value', inject(function (params) {
+        params('key', 'value1');
+        params('key', 'value2');
+        expect(params('key')).toBe('value2');
+    }));
+});

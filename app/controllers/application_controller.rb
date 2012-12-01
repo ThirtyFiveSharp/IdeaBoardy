c1fa2d0289bdf61fe4023f12fdf5eb280dea3aac
swzhou <swing.zhou@hotmail.com>
2012-12-01 15:47:33 +0800

    [SW] add sign-in and sign-out navigation logic for admin user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,4 +13,8 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     admin_path
   end
+
+  def after_sign_out_path_for(resource)
+    new_user_session_path
+  end
 end

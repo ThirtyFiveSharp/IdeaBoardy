4a4880050dcd40f1327a09589895a0b38cc3662c
swzhou <swing.zhou@hotmail.com>
2012-12-01 20:18:18 +0800

    [SW] quick fix for home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @setting = Admin::Setting.first
+    #@setting = Admin::Setting.first
+    @setting = Admin::Setting.new(slogan: "Let's move!", app_version: "0.0.2-Alpha")
   end
 end
--- test/functional/home_controller_test.rb
+++ test/functional/home_controller_test.rb
@@ -8,7 +8,8 @@ class HomeControllerTest < ActionController::TestCase
   test "should get index" do
     get :index
     assert_response :success
-    assert_equal @setting, assigns(:setting)
+    assert_equal @setting.slogan, assigns(:setting).slogan
+    assert_equal @setting.app_version, assigns(:setting).app_version
   end
 
 end

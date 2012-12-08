eafa70cd504a0ed66764e83643d640010cc4ac25
Tong Zhang <kanetzhang@gmail.com>
2012-12-09 00:43:36 +0800

    [ZT] fix jt

--- spec/javascripts/controllers/board-controller-spec.js
+++ spec/javascripts/controllers/board-controller-spec.js
@@ -2,10 +2,12 @@ describe('BoardController', function () {
     var scope, ctrl, $httpBackend,
         boardUri = 'http://localhost:3000/api/boards/1',
         sectionsLinkUri = boardUri + "/sections",
+        tagsLinkUri = boardUri + "/tags",
         reportUri = boardUri + "/report",
         board = {"id":1, "name":"tiger retro", "description":"tiger retro for iteration 29", "links":[
             {"rel":"self", "href":boardUri},
             {"rel":"sections", "href":sectionsLinkUri},
+            {"rel":"tags", "href":tagsLinkUri},
             {"rel":"report", "href":reportUri}
         ]},
         sections = [
@@ -23,6 +25,7 @@ describe('BoardController', function () {
         params('uri', boardUri);
         $httpBackend = _$httpBackend_;
         $httpBackend.expectGET(boardUri).respond(board);
+        $httpBackend.expectGET(tagsLinkUri).respond(sections);
         $httpBackend.expectGET(sectionsLinkUri).respond(sections);
         scope = $rootScope.$new();
         scope.idea = board;
@@ -40,7 +43,8 @@ describe('BoardController', function () {
             expect(scope.board).toBe(board);
             expect(scope.board.selfLink).toBe(board.links[0]);
             expect(scope.board.sectionsLink).toBe(board.links[1]);
-            expect(scope.board.reportLink).toBe(board.links[2]);
+            expect(scope.board.tagsLink).toBe(board.links[2]);
+            expect(scope.board.reportLink).toBe(board.links[3]);
             expect(scope.board.mode).toBe('view');
             expect(typeof scope.board.edit).toBe('function');
             expect(typeof scope.board.delete).toBe('function');

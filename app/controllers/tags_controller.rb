a5bea3921576b2f0a4816f42123780adf055c1af
Tong Zhang <kanetzhang@gmail.com>
2012-12-09 00:06:22 +0800

    [ZT] fix auto merging issue

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -19,53 +19,45 @@ class TagsController < ApplicationController
   end
 
   def create
-    until_found do
-      board_id = params[:board_id]
-      board = Board.find board_id
-      @tag = Tag.new(params[:tag])
-      @tag.board = board
-      if @tag.save
-        head status: :created, location: board_tag_url(board_id, @tag.id)
-      else
-        render json: @tag.errors, status: :unprocessable_entity
-      end
+    board_id = params[:board_id]
+    board = Board.find board_id
+    @tag = Tag.new(params[:tag])
+    @tag.board = board
+    if @tag.save
+      head status: :created, location: board_tag_url(board_id, @tag.id)
+    else
+      render json: @tag.errors, status: :unprocessable_entity
     end
   end
 
   def show
     board_id = params[:board_id]
     tag_id = params[:id]
-    until_found do
-      @tag = Tag.of_board(board_id).find(tag_id)
-      render json: {
-          id: @tag.id,
-          name: @tag.name,
-          links: [
-              {rel: 'self', href: board_tag_url(board_id, tag_id)}
-          ]
-      }
-    end
+    @tag = Tag.of_board(board_id).find(tag_id)
+    render json: {
+        id: @tag.id,
+        name: @tag.name,
+        links: [
+            {rel: 'self', href: board_tag_url(board_id, tag_id)}
+        ]
+    }
   end
 
   def update
     board_id = params[:board_id]
     tag_id = params[:id]
-    until_found do
-      @tag = Tag.of_board(board_id).find(tag_id)
-      if @tag.update_attributes(params[:tag])
-        head :no_content
-      else
-        render json: @tag.errors, status: :unprocessable_entity
-      end
+    @tag = Tag.of_board(board_id).find(tag_id)
+    if @tag.update_attributes(params[:tag])
+      head :no_content
+    else
+      render json: @tag.errors, status: :unprocessable_entity
     end
   end
 
   def destroy
     tag_id = params[:id]
-    until_found :no_content do
-      @section = Tag.find(tag_id)
-      @section.destroy
-      head :no_content
-    end
+    @section = Tag.find(tag_id)
+    @section.destroy
+    head :no_content
   end
 end

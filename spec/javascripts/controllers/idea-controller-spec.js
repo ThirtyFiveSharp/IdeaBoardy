09f39777cbb0a9df932cf78dcff4dcb86fdb2dad
Tong Zhang <kanetzhang@gmail.com>
2012-12-10 00:43:50 +0800

    [ZT] fix jt

--- spec/javascripts/controllers/idea-controller-spec.js
+++ spec/javascripts/controllers/idea-controller-spec.js
@@ -5,20 +5,33 @@ describe('IdeaController', function () {
     describe("initialize", function () {
         var scope, ctrl, $httpBackend,
             ideaUri = 'http://localhost:3000/api/boards/1/sections/1/ideas/1',
-            ideaRef = {"id":1, "content":"idea content", "vote":3, "links":[
-                {"rel":"idea", "href":ideaUri}
-            ]};
+            ideaTagsUri = ideaUri + '/tags',
+            ideaRef = {
+                "links":[
+                    {"rel":"idea", "href":ideaUri}
+                ]
+            },
+            idea = {
+                "id":1,
+                "content":"idea content",
+                "vote":3,
+                "links":[
+                    {"rel":"self", "href":ideaUri},
+                    {"rel":"tags", "href":ideaTagsUri}
+                ]
+            };
 
         beforeEach(inject(function (_$httpBackend_, $rootScope, $controller) {
             $httpBackend = _$httpBackend_;
-            $httpBackend.expectGET(ideaUri).respond(ideaRef);
+            $httpBackend.expectGET(ideaUri).respond(idea);
+            $httpBackend.expectGET(ideaTagsUri).respond([]);
             scope = $rootScope.$new();
             scope.idea = ideaRef;
             ctrl = $controller('IdeaController', {$scope:scope});
+            $httpBackend.flush();
         }));
 
         it("should enhance idea", function () {
-            $httpBackend.flush();
             var addVoteFn = scope.idea.addVote;
             expect(addVoteFn).not.toBe(null);
             expect(typeof addVoteFn).toBe('function');

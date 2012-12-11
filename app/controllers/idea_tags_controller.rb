27b225e0d2fd675bd8e0b2ab10cb26d84e6d5f34
Tong Zhang <kanetzhang@gmail.com>
2012-12-11 23:28:41 +0800

    [ZT] remove unused code

--- app/controllers/idea_tags_controller.rb
+++ app/controllers/idea_tags_controller.rb
@@ -19,19 +19,4 @@ class IdeaTagsController < ApplicationController
       }
     }
   end
-
-  def update
-    board_id = params[:board_id]
-    section_id = params[:section_id]
-    idea_id = params[:id]
-    return head(:not_found) unless Section.of_board(board_id).exists?(section_id)
-    return head(:not_found) unless Idea.of_section(section_id).exists?(idea_id)
-    @idea = Idea.find(idea_id)
-    if @idea.update_attributes(params[:idea])
-      head :no_content
-    else
-      render json: @idea.errors, status: :unprocessable_entity
-    end
-  end
-
 end

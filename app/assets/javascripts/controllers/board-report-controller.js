187ab99e5be50dfbad4a18df2f311b959ea86ec0
swzhou <swing.zhou@hotmail.com>
2013-01-23 21:56:56 +0800

    [SW] fix random sections sequence at report page

--- app/assets/javascripts/controllers/board-report-controller.js
+++ app/assets/javascripts/controllers/board-report-controller.js
@@ -14,11 +14,11 @@ angular.module('idea-boardy')
             $http.get(config.shortenUrlEntryPoint + "/" + params('shortenUrlCode'), {params:{embed:"sections"}})
                 .success(function (board) {
                     $scope.board = board;
-                    $scope.sections = [];
-                    _.each($scope.board.sections, function (section) {
+                    $scope.sections = board.sections;
+                    _.each($scope.board.sections, function (section, index) {
                         $http.get(section.links.getLink('self').href, {params:{embed:"ideas"}})
                             .success(function (section) {
-                                $scope.sections.push(section);
+                                $scope.sections[index] = section;
                             });
                     });
                 });

542c4ee4468b7cb02e432675b0feaeb7d7dbd8ab
swzhou <swing.zhou@hotmail.com>
2013-02-12 20:58:23 +0800

    [SW] show board details

--- app/assets/javascripts/mobile/controllers/m-board-details-controller.js
+++ app/assets/javascripts/mobile/controllers/m-board-details-controller.js
@@ -1,11 +1,19 @@
 angular.module('m-idea-boardy')
     .controller('MBoardDetailsController', ['$scope', '$http', 'config', 'params',
     function ($scope, $http, config, params) {
-        $scope.initialize = function() {
+        $scope.initialize = function () {
             var boardUri = config.shortenUrlEntryPoint + "/" + params('shortenUrlCode');
-            $http.get(boardUri, {params:{embed:"tags,concepts"}}).success(function (board) {
-                $scope.board = board;
-            });
+            $http.get(boardUri, {params:{embed:"sections"}})
+                .success(function (board) {
+                    $scope.board = board;
+                    $scope.sections = [];
+                    _.each($scope.board.sections, function (section, index) {
+                        $http.get(section.links.getLink('self').href, {params:{embed:"ideas"}})
+                            .success(function (section) {
+                                $scope.sections[index] = section;
+                            });
+                    });
+                });
         };
     }
 ]);

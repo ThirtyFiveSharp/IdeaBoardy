767abb6a5793ec6fd26bebd16c7c0a5110ec1224
swzhou <swing.zhou@hotmail.com>
2013-02-13 10:46:34 +0800

    [SW] slight style change

--- app/assets/javascripts/mobile/controllers/m-board-details-controller.js
+++ app/assets/javascripts/mobile/controllers/m-board-details-controller.js
@@ -1,6 +1,6 @@
 angular.module('m-idea-boardy')
-    .controller('MBoardDetailsController', ['$scope', '$http', 'config', 'params',
-    function ($scope, $http, config, params) {
+    .controller('MBoardDetailsController', ['$scope', '$http', '$history', 'config', 'params',
+    function ($scope, $http, $history, config, params) {
         $scope.initialize = function () {
             var boardUri = config.shortenUrlEntryPoint + "/" + params('shortenUrlCode');
             $http.get(boardUri, {params:{embed:"sections"}})
@@ -18,10 +18,14 @@ angular.module('m-idea-boardy')
         $scope.vote = function(idea, section) {
             $http.post(idea.links.getLink('vote').href).success(function() {
                 $http.get(section.links.getLink('self').href, {params:{embed:"ideas"}})
-                    .success(function (section1) {
-                        section.ideas = section1.ideas;
+                    .success(function (refreshedSection) {
+//                        section.ideas = refreshedSection.ideas;
+                        angular.extend(section, refreshedSection);
                     });
             });
         };
+        $scope.hasHistory = function() {
+            return $history.activeIndex > 0;
+        }
     }
 ]);
